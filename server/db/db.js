24caae980903a059922d2a3b781e142bc91905f4
Sunjin Kang <Sunjin@bu.edu>
2018-03-16 17:19:15 -0400

    d

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 //     logging: false
 //   }
 // )
-const db = new Sequelize(process.env.DATABASE_URL || 'johnjsanchez', 'postgres', 'postgres', { dialect: 'postgres' })
+const db = new Sequelize(process.env.DATABASE_URL || 'johnjsanchez', 'iehvvuormywzhw', '9327e659b2f4b3073a8226b18966dc5f40e26313c1649ae6416acb64f2fd10b2', { dialect: 'postgres' })
 db.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.')
